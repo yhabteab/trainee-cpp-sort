cf3c163ba84c15929427e43a8fbf06e40cc894c4
Yonas Habteab <57616252+Yonas-net@users.noreply.github.com>
2020-05-20 20:17:02 +0200

    Add parameter --numeric-sort (#15)

--- main.cpp
+++ main.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-#include <cstring>
-
+#include <unistd.h>
+#include <boost/lexical_cast.hpp>
 
 using namespace std;
 
-void bubbleSort(vector<string> &arr, size_t length);
+void bubbleSort(vector<string> &arr, size_t length,
+                bool (*funcCallback)(string &, string &));
 
 void printSortedArray(const vector<string> &arr);
 
+bool checkForEquality(string &firstNum, string &secondNum);
+
+bool checkNumeric(string &firstNum, string &secondNum);
+
 int main(int argc, char **argv) {
     vector<string> stringVector;
+    int $options{};
+    bool (* funcCallback)(string &, string &) = checkForEquality;
 
-    if (argc > 1 && strcmp(argv[1], "-") != 0) {
-        ifstream file{argv[1]};
+    while (($options = getopt(argc, argv, "n")) != -1) {
+        switch ($options) {
+            case 'n':
+                funcCallback = checkNumeric;
+                break;
+            default: /* For invalid option e.g '?' */
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    if (argv[optind] != nullptr) {
+        ifstream file{argv[optind]};
         for (string line; getline(file, line);) {
             stringVector.emplace_back(line);
         }
@@ -26,19 +43,22 @@ int main(int argc, char **argv) {
         }
     }
 
-    bubbleSort(stringVector, stringVector.size());
+    size_t vectorSize = stringVector.size();
+
+    bubbleSort(stringVector, vectorSize, funcCallback);
     printSortedArray(stringVector);
 
     return EXIT_SUCCESS;
 }
 
-void bubbleSort(vector<string> &arr, size_t length) {
+void bubbleSort(vector<string> &arr, size_t length,
+                bool (*funcCallback)(string &, string &)) {
     bool isSwapped = true;
 
     for (size_t i{0}; (i < length - 1) && (isSwapped); i++) {
         isSwapped = false;
         for (size_t j{0}; j < (length - i) - 1; j++) {
-            if (arr.at(j + 1) < arr.at(j)) {
+            if (funcCallback(arr.at(j + 1), arr.at(j))) {
                 swap(arr.at(j + 1), arr.at(j));
 
                 isSwapped = true;
@@ -51,4 +71,12 @@ void printSortedArray(const vector<string> &arr) {
     for (const auto &num: arr) {
         cout << num << endl;
     }
+}
+
+bool checkForEquality(string &firstNum, string &secondNum) {
+    return firstNum < secondNum;
+}
+
+bool checkNumeric(string &firstNum, string &secondNum) {
+    return boost::lexical_cast<int>(firstNum) < boost::lexical_cast<int>(secondNum);
 }
